9d6140dfd72f84fdcc88fcfdab4a21518ee67ab6
kmpujar <karthikpujar737@gmail.com>
2019-01-21 22:22:41 -0500

    First Commit

---
+++ task2.py
@@ -0,0 +1,78 @@
+from __future__ import division
+import pandas as pd
+from conll_df import conll_df
+from collections import Counter
+from sklearn.metrics import precision_recall_fscore_support
+
+class tokenWord:
+    def __init__(self, word, feature, count):
+        self.word=word
+        self.feature=feature
+        self.count=count
+
+#def loadData(self,fileList)
+filePre='./Data/'
+fileList=['UD_English-EWT/en_ewt-um-train.conllu']
+list1=[2,5]
+for file in fileList:
+    with open(filePre+file) as fin:
+         rows = ( line.split('\t') for line in fin if line[0]!='#' and line[0]!=' ' )
+         data = [ row[0:] for row in rows ]
+
+countDict={}
+fcountDict={}
+uList=[]
+wordList=[]
+for item in data:
+    if(len(item)>5):
+        itemList=item[5].split(';')
+        itemList.sort()
+        feature=';'.join(itemList)
+        uList.append((item[2].lower(),feature))
+
+countList=dict(Counter(uList))
+
+print(len(countList))
+
+for i in countList:
+    change=0
+    for x in wordList:
+        if x.word == i[0]:
+            change=1
+            if(x.count<countList[i]):
+                x.count=countList[i]
+                x.feature=i[1]
+                    #print(x.word, i[0],x.feature,x.count,countList[i])
+                break
+    if change==0:
+        wordList.append(tokenWord(i[0],i[1],countList[i]))
+
+filePre='./Data/'
+fileList=['UD_English-EWT/en_ewt-um-dev.conllu']
+list1=[2,5]
+for file in fileList:
+    with open(filePre+file) as fin:
+         rows = ( line.split('\t') for line in fin if line[0]!='#' and line[0]!=' ' )
+         data = [ row[0:] for row in rows ]
+instance,correct=0,0
+uList=[]
+y_true=[]
+y_pred=[]
+for item in data:
+    if(len(item)>5):
+        instance+=1
+        itemList=item[5].split(';')
+        itemList.sort()
+        item[5]=';'.join(itemList)
+        searchResult=next((x for x in wordList if x.word ==item[2].lower() ), None)
+        if(searchResult is not None):
+            y_true.append(item[5])
+            y_pred.append(searchResult.feature)
+            if(searchResult.feature==item[5]):
+                correct+=1
+            y_true.append(item[5])
+            y_pred.append(searchResult.feature)
+print(correct/instance)
+print(precision_recall_fscore_support(y_true, y_pred, average='micro'))
+
+# precision recall and F1 to be calculated
